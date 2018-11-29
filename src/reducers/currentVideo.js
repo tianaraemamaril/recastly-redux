62d9c7d359c05d20151c68e4532d00cc13238be1
tianaraemamaril <tianaraetran@gmail.com>
2018-11-28 16:59:01 -0800

    Implement reducers - currentVideo/videoList

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,10 +1,13 @@
 import Redux from 'redux';
+import changeVideo from '../actions/currentVideo.js';
 
-var currentVideoReducer = (state = null, action) => {
+var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
-  if (action.type === 'CHANGE_VIDEO') {
 
+  if (action.type === 'CHANGE_VIDEO') {
+    return action.video;
   }
+  return null;
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,10 +1,13 @@
 import Redux from 'redux';
+import changeVideoList from '../actions/videoList.js';
 
-var videoListReducer = (state = [], action) => {
+var videoListReducer = (state = [], action) => { // does state need a default here??
+
   //TODO: define a reducer for the videoList field of our state.
   if (action.type === 'CHANGE_VIDEO_LIST') {
-
+    return action.videos;
   }
+  return [];
 };
 
 export default videoListReducer;
