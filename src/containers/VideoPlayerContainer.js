b6584f3cffa193dd53db3d18f3dc8f8849006de4
tianaraemamaril <tianaraetran@gmail.com>
2018-11-28 19:59:05 -0800

    Prepare for .commit implementation

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
+import currentVideoReducer from '../reducers/currentVideo.js';
 
-var VideoPlayerContainer = () => {};
+var VideoPlayerContainer = () => { };
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
 
-export default VideoPlayerContainer;
+const mapDispatchToProps = {
+  currentVideoReducer
+};
+
+export default VideoPlayerContainer;// connect(mapStateToProps, mapDispatchToProps VideoPlayerContainer;
